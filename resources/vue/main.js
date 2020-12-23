eb117e6dbf7c6c4f2af6e0b8448377a721ef2eda
Mouctar DIALLO <abdoulmouctard@gmail.com>
2020-12-23 15:39:28 +0100

    tailwind css setup

--- resources/vue/main.js
+++ resources/vue/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 import router from "./router";
 
+import "./assets/styles/index.css";
+
 Vue.use(VueRouter);
 
+Vue.config.productionTip = false;
+
 new Vue({
-    router
-}).$mount('#app');
+    router,
+}).$mount("#app");
